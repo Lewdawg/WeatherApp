85ab22de979b1e42ac7516d6af2c36a6442fe4a6
Lewdawg <Leweswalton@gmail.com>
2021-04-18 10:49:32 +0200

    lat & longitude

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
-
 import './App.css';
 
+import { useState, useEffect } from 'react'
+
 function App() {
+
+  //Latitude & Longitude
+  const [lat, setLat] = useState([]);
+  const [long, setLong] = useState([]);
+
+
+  //We get our latitude and longitude using navigator.geolocation 
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      setLat(position.coords.latitude);
+      setLong(position.coords.longitude);
+    });
+
+    console.log('Latitude is:', lat)
+    console.log('Longitude is:', long)
+
+  }, [lat, long]);
 
 
   return (
